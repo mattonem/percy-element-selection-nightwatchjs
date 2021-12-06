a19f545871a70863b55698025e206d8dcd403b3f
maxmattone <max.m@browserstack.com>
2021-12-06 15:13:02 +0000

    remove subsclassing

--- commands/percySnapshotForSelectedElements.js
+++ commands/percySnapshotForSelectedElements.js
@@ -1,8 +1,6 @@
-const utils = require('@percy/sdk-utils');
 const PercySnapshotCommand = require('@percy/nightwatch')
-let log = utils.logger('nightwatch');
 
-module.exports = class PercySnapshotForSelectedElementsCommand extends PercySnapshotCommand{
+module.exports = class PercySnapshotForSelectedElementsCommand{
   async command(name, options) {
     await this.api.execute(`for (let el of document.querySelector('${options.elements}').getElementsByTagName("*")) {el.classList.add("visible-in-percy")}`);
     await this.api.execute(`document.querySelector('${options.elements}').classList.add("visible-in-percy")`);
@@ -13,9 +11,9 @@ module.exports = class PercySnapshotForSelectedElementsCommand extends PercySnap
     optionCopy.percyCSS = options.percyCSS?options.percyCSS:''
     optionCopy.percyCSS += '*:not(.visible-in-percy) { visibility: hidden; } .visible-in-percy {visibility: visible;}'
     delete optionCopy.elements
-    let result = await super.command(name, optionCopy)
+    let snapshot = await new PercySnapshotCommand().command.call(this, name, optionCopy)
     await this.api.execute(`document.querySelector('.visible-in-percy').classList.remove("visible-in-percy")`);
-    return result
+    return this.api
   }
 
 }
