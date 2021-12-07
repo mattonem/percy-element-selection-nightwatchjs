19e172fce813ea69a2ce97734e0e59e946a24b62
maxmattone <max.m@browserstack.com>
2021-12-07 09:38:41 +0000

    more robuste tests

--- tests/shouldKeepPercyCss.js
+++ tests/shouldKeepPercyCss.js
@@ -2,6 +2,6 @@ test('Browserstack signup button with CSS injection', function (browser) {
   browser
     .url("https://www.browserstack.com")
     .percySnapshotForSelectedElements('BS singup with css 1', {elements:'#signupModalButton', percyCSS:'#signupModalButton {background-color: #a52a2a}'})
-    .percySnapshotForSelectedElements('BS singup with css 2', {elements:'#signupModalButton', percyCSS:'#signupModalButton {background-color: #99cc00}'})
+    .percySnapshotForSelectedElements('BS freetrial with css 2', {elements:'#free-trial-link-anchor', percyCSS:'#free-trial-link-anchor {background-color: #99cc00}'})
     .expect.title().to.match(/BrowserStack/i);
 });
